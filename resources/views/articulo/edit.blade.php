e34d44fa236cfac42ad796bad582105268719273
oscarm24 <oscarmontoyasr001@gmail.com>
2024-11-03 22:25:25 -0600

    first commit

---
+++ resources/views/articulo/edit.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.plantillabase');
+
+@section('contenido')
+<h2>EDITAR ARTICULOS</h2>
+
+<form action="/articulos/{{$articulo->id}}" method="POST">
+    @csrf
+    @method('PUT')
+    <div class="mb-3">
+        <label for="" class="form-label">Codigo</label>
+        <input id="codigo" name="codigo" type="text" class="form-control" tabindex="1" value="{{$articulo->codigo}}">
+    </div>
+    <div class="mb-3">
+        <label class="form-label">Descripcion</label>
+        <input id="descripcion" name="descripcion" type="text" class="form-control" tabindex="2" value="{{$articulo->descripcion}}">
+    </div>
+    <div class="mb-3">
+        <label class="form-label">Cantidad</label>
+        <input id="cantidad" name="cantidad" type="number" class="form-control" tabindex="3" value="{{$articulo->cantidad}}">
+    </div>
+    <div class="mb-3">
+        <label class="form-label">Precio</label>
+        <input id="precio" name="precio" type="number" step="any" value="0.00" class="form-control" tabindex="4" value="{{$articulo->precio}}">
+    </div>
+    <a href="/articulos" class="btn btn-secondary" tabindex="5">Cancelar</a>
+    <button type="submit" class="btn btn-primary" tabindex="4">Guardar</button>
+</form>
+@endsection
